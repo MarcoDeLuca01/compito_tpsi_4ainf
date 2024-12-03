803a630b662ff15237990751e555d3bd3279bf43
MarcoDeLuca01 <marco.deluca@davincimilazzo.edu.it>
2024-12-03 13:32:39 +0100

    Add files via upload
    Marco De Luca 4a inf consegna 13.32 giorno 03/12/2024

---
+++ math.c
@@ -0,0 +1,19 @@
+'int somma(int, int) : esegue la somma tra due interi e restituisce il risultato [0.5 pt]
+int differenza(int, int) : esegue la differenza tra due interi e restituisce il risultato [0.5 pt]
+int moltiplicazione(int, int) : eseuge la moltiplicazoine tra due interi e restituisce il risultato [0.5 pt]
+flaot divisione(float, float) : esegue la divisione tra due float e restituisce il risultato [0.5 pt]'
+
+int somma(int x, int y){
+    int r;
+    r=x+y;
+    printf("La somma e' %d",r)
+}
+int differenza(int x, int y){
+
+}
+int moltiplicazione(int x, int y){
+
+}
+float divisione(float, float){
+
+}
---
+++ math.h
@@ -0,0 +1,4 @@
+int somma(int x, int y);
+int differenza(int x, int y);
+int moltiplicazione(int x, int y);
+float divisione(float x, float y);
